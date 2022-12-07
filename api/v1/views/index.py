2c2f9468b68c556cc3e87ff2354835f518c505c6
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-12-07 18:23:37 +0000

    Fix file index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,13 @@
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
-
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 hbnbText = {
     "amenities": "Amenity",
@@ -24,7 +30,11 @@ def hbnbStatus():
 @app_views.route('/stats', strict_slashes=False)
 def hbnbStats():
     """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
+    return {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
