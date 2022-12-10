53a212b968d0b8942a22b282a22e9fa39502daba
santialvesz <santialvesdsc@gmail.com>
2022-12-10 14:57:51 -0300

    Taask 12 fix with objects

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -26,7 +26,7 @@ def get_reviews(place_id):
                  strict_slashes=False)
 def get_review(review_id):
     """get review information for specified review"""
-    review = storage.get("Review", review_id)
+    review = storage.get(Review, review_id)
     if review is None:
         abort(404)
     return jsonify(review.to_dict())
@@ -36,7 +36,7 @@ def get_review(review_id):
                  strict_slashes=False)
 def delete_review(review_id):
     """deletes a review based on its review_id"""
-    review = storage.get("Review", review_id)
+    review = storage.get(Review, review_id)
     if review is None:
         abort(404)
     review.delete()
@@ -48,7 +48,7 @@ def delete_review(review_id):
                  strict_slashes=False)
 def post_review(place_id):
     """create a new review"""
-    place = storage.get("Place", place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     if not request.get_json():
@@ -56,7 +56,7 @@ def post_review(place_id):
     kwargs = request.get_json()
     if 'user_id' not in kwargs:
         return make_response(jsonify({'error': 'Missing user_id'}), 400)
-    user = storage.get("User", kwargs['user_id'])
+    user = storage.get(User, kwargs['user_id'])
     if user is None:
         abort(404)
     if 'text' not in kwargs:
@@ -71,7 +71,7 @@ def post_review(place_id):
                  strict_slashes=False)
 def put_review(review_id):
     """update a review"""
-    review = storage.get("Review", review_id)
+    review = storage.get(Review, review_id)
     if review is None:
         abort(404)
     if not request.get_json():
